5f89e1d2373dc1d03605066686a3a10f89c1e45b
RGC02995 <raulgc2995@gmail.com>
2025-10-03 19:25:16 +0200

    add: btn add new propertie

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 export const Home = () => {
   return (
-    <div className="bg-neutral-100 p-4 m-5 flex-1 rounded-md border-2 border-neutral-100 h-[90vh]">
-      <h1 className="text-neutral-400 ">Propiedades</h1>
+    <div className="bg-neutral-100 p-4 m-8 flex-1 flex-wrap  rounded-md border-2 border-neutral-100 h-[90vh]">
+      <h1 className="text-neutral-400  font-semibold ">Propiedades</h1>
+      <button className="bg-neutral-950 text-white px-4 py-2 rounded-md mt-4 hover:bg-neutral-500 flex items-center gap-2 flex-wrap">
+        <i>+</i>Añadir nueva propiedad
+      </button>
     </div>
   );
 };
