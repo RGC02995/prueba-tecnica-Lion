0ae5c7563dc404934b411398bf5dbc28c37f519c
RGC02995 <raulgc2995@gmail.com>
2025-10-03 19:52:12 +0200

    add: style responsive component -> Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 export const Home = () => {
   return (
-    <div className="bg-neutral-100 p-4 m-8 flex-1 flex-wrap  rounded-md border-2 border-neutral-100 h-[90vh]">
-      <h1 className="text-neutral-400  font-semibold ">Propiedades</h1>
-      <button className="bg-neutral-950 text-white px-4 py-2 rounded-md mt-4 hover:bg-neutral-500 flex items-center gap-2 flex-wrap">
-        <i>+</i>Añadir nueva propiedad
+    <div className="m-5 bg-white w-[90vw] h-[90vh] my-5 p-6 rounded-lg shadow-lg flex flex-col sm:flex-row sm:justify-between sm:items-center">
+      {/*Título*/}
+      <h1 className="text-neutral-800 text-2xl sm:text-3xl font-semibold mb-4 sm:mb-0">
+        Propiedades
+      </h1>
+      {/*Botón*/}
+      <button className="bg-neutral-950 text-white px-4 py-2 rounded-md text-sm sm:text-base hover:bg-neutral-700 flex items-center gap-2 self-start sm:self-auto">
+        <span className="text-xl font-bold">+</span>
+        Añadir nueva propiedad
       </button>
     </div>
   );
