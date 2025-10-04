ec89a3b258b061b68c908701ffa9089e6a3e4952
RGC02995 <raulgc2995@gmail.com>
2025-10-04 19:21:07 +0200

    fix: rename BtnSearchProperty to BtnAddProperty

--- src/components/features/PropertyList.jsx
+++ src/components/features/PropertyList.jsx
@@ -1,21 +1,49 @@
+import propertiesData from "../../data/properties.json";
+
 export const PropertyList = () => {
   return (
-    <div className=" mt-4 overflow-auto border border-neutral-300 rounded-md max-h-[60vh]">
-      <table className="w-full table-auto border-collapse">
+    <div className=" mt-4 overflow-x-auto min-w-[300px] rounded-md  max-h-[60vh] sm:font-medium font-light">
+      <table className="w-full border-collapse text-sm">
         {/* Header */}
-        <thead className="bg-gray-100 ">
-          <tr className="p-1">
-            <th className=" text-left">Foto</th>
-            <th className=" text-left">Oficina</th>
-            <th className=" text-left">Referencia</th>
-            <th className=" text-left">Tipo</th>
-            <th className=" text-left">Dirección</th>
-            <th className=" text-left">Precio</th>
-            <th className=" text-left">Habitaciones</th>
-            <th className=" text-left">Superficie</th>
-            <th className=" text-left">Fecha</th>
+        <thead className="bg-gray-200 font-thin text-gray-700">
+          <tr>
+            <th className="p-2 text-center">Foto</th>
+            <th className="p-2 text-center">Oficina</th>
+            <th className="p-2 text-center">Referencia</th>
+            <th className="p-2 text-center">Tipo</th>
+            <th className="p-2 text-center">Dirección</th>
+            <th className="p-2 text-center">Precio</th>
+            <th className="p-2 text-center">Habitaciones</th>
+            <th className="p-2 text-center">Superficie</th>
+            <th className="p-2 text-center">Fecha</th>
           </tr>
         </thead>
+
+        {/*Body*/}
+        <tbody className="">
+          {propertiesData.map((property) => (
+            <tr
+              key={property.id}
+              className="border-t border-black  text-center m-auto"
+            >
+              <td className="p-4">
+                <img
+                  src={property.images}
+                  alt="imagen random"
+                  className=" w-30 min-w-20 rounded-md"
+                />
+              </td>
+              <td className="p-4">{property.office}</td>
+              <td className="p-4">{property.reference}</td>
+              <td className="p-4">{property.type}</td>
+              <td className="p-4">{property.location}</td>
+              <td className="p-4">{property.price}€</td>
+              <td className="p-4">{property.rooms}</td>
+              <td className="p-4">{property.area} m2</td>
+              <td className="p-4">{property.date}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
--- src/components/ui/BtnSearchPropertie.jsx
+++
@@ -1,8 +0,0 @@
-export const BtnSearchPropertie = () => {
-  return (
-    <button className="bg-neutral-950 text-white px-4 py-2 rounded-md text-sm sm:text-base hover:bg-neutral-700 flex items-center gap-2 self-start sm:self-auto">
-      <span className="text-xl font-bold">+</span>
-      Añadir nueva propiedad
-    </button>
-  );
-};
