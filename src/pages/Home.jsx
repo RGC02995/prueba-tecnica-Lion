0344176c7cea8844ee751c6ce001ce3786d23e1c
RGC02995 <raulgc2995@gmail.com>
2025-10-04 17:41:01 +0200

    feat: add component BtnSearch Propertie in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,19 @@
 import { SearchInput } from "../components/ui/SearchInput";
 import { PropertyList } from "../components/features/PropertyList";
+import { BtnSearchPropertie } from "../components/ui/BtnSearchPropertie";
 
 export const Home = () => {
   return (
     <div className="w-full min-h-screen bg-neutral-100 flex flex-col items-center py-6">
       {/* Ventana blanca */}
-      <div className="bg-white w-[90vw] h-[90vh] p-6 rounded-lg shadow-lg flex flex-col">
+      <div className=" w-[90vw] h-[90vh] p-6 rounded-lg shadow-lg flex flex-col">
         {/* Header: Título + Botón */}
         <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center mb-4">
           <h1 className="text-neutral-800 text-lg sm:text-2xl font-semibold mb-2 sm:mb-0">
             Propiedades
           </h1>
-          <button className="bg-neutral-950 text-white px-4 py-2 rounded-md text-sm sm:text-base hover:bg-neutral-700 flex items-center gap-2 self-start sm:self-auto">
-            <span className="text-xl font-bold">+</span>
-            Añadir nueva propiedad
-          </button>
+          {/*Boton */}
+          <BtnSearchPropertie />
         </div>
         {/* Buscador */}
         <SearchInput />
